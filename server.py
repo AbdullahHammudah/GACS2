8ab69416f9a1846b9e465b98c90d87db10dde949
Abdullah Hammudah <hammudah.active@gmail.com>
2024-06-11 09:48:02 +0300

    changing  voice socket to UDP - first trial

--- server.py
+++ server.py
@@ -15,54 +15,47 @@ close_voice_connection = False
 
 control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 control_socket.bind(ADDR_CONTROL)
-voice_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+voice_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 voice_socket.bind(ADDR_VOICE)
 
 def broadcast(data, except_client=None):
     for client in clients:
         if client != except_client:
             try:
-                client.sendall(data)
+                voice_socket.sendto(data, client)
             except Exception as e:
                 print(f"Error sending data to client: {e}")
                 clients.remove(client)
 
-def start_voice_channel(control_addr, voice_conn, voice_addr):
+def start_voice_channel(control_addr):
     global close_voice_connection
-    if control_addr[0] == voice_addr[0]:
-        voice_connection = True
-        while voice_connection:
+    if control_addr[0]:
+        while not close_voice_connection:
             try:
-                if close_voice_connection:
-                    break
-                voice_data = voice_conn.recv(1024)
+                voice_data, client_addr = voice_socket.recvfrom(1024)
                 if not voice_data:
                     break
-                broadcast(voice_data, except_client=voice_conn)
+                broadcast(voice_data, except_client=client_addr)
             except Exception as e:
-                print(f"Client Finsihed Speaking {voice_addr}: {e}")
+                print(f"Client Finished Speaking {client_addr}: {e}")
                 break
 
-
-def handle_client (control_conn, control_addr, voice_conn, voice_addr):
+def handle_client(control_conn, control_addr):
     global close_voice_connection
     global speaking_client
-    print(f"[CONTROL CONNECTION] Client {control_addr} has establised Contol Connection")
-    print(f"[VOICE CONNECTION] Client {voice_addr} has establised Voice Connection")
-
+    print(f"[CONTROL CONNECTION] Client {control_addr} has established Control Connection")
    
-    try: 
+    try:
         connected = True
         while connected:
             control_msg = control_conn.recv(1024).decode(FORMAT)
             print(f"[{control_addr}] {control_msg}")
-            if control_msg == 'S' and speaking_client == None:
+            if control_msg == 'S' and speaking_client is None:
                 control_conn.send(b"Permission to speak granted")
                 speaking_client = control_conn
                 close_voice_connection = False
-                voice_thread = threading.Thread(target=start_voice_channel, args=(control_addr, voice_conn, voice_addr))
+                voice_thread = threading.Thread(target=start_voice_channel, args=(control_addr,))
                 voice_thread.start()
-
 
             elif control_msg == 'F':
                 control_conn.send(b"Speaking is Over")
@@ -73,16 +66,12 @@ def handle_client (control_conn, control_addr, voice_conn, voice_addr):
     except Exception as e:
         print(f"Connection Problem {e}")
 
-
 def start():
     control_socket.listen()
-    voice_socket.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
         control_conn, control_addr = control_socket.accept()
-        voice_conn, voice_addr = voice_socket.accept()
-        threading.Thread(target=handle_client, args=(control_conn, control_addr, voice_conn, voice_addr)).start()
-        clients.append(voice_conn)
+        threading.Thread(target=handle_client, args=(control_conn, control_addr)).start()
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
 print("[STARTING] server is starting...")
